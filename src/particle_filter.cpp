033598f83ebc67d4a38998d1a23a8fadf24d4405
Edwin Martinez <martinez92edwin@gmail.com>
2019-08-12 11:59:16 -0700

    initialized particles

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -21,17 +21,38 @@
 using std::string;
 using std::vector;
 
+static default_random_engine gen;
+
 void ParticleFilter::init(double x, double y, double theta, double std[]) {
   /**
-   * TODO: Set the number of particles. Initialize all particles to 
-   *   first position (based on estimates of x, y, theta and their uncertainties
-   *   from GPS) and all weights to 1. 
+   * TODO: Set the number of particles.
    * TODO: Add random Gaussian noise to each particle.
    * NOTE: Consult particle_filter.h for more information about this method 
    *   (and others in this file).
    */
-  num_particles = 0;  // TODO: Set the number of particles
+  num_particles = 100;  // TODO: Set the number of particles
+  /*
+  Initialize all particles to 
+   *   first position (based on estimates of x, y, theta and their uncertainties
+   *   from GPS) and all weights to 1. 
+   assuming std[] -> std[std_x, std_y, std_theta]
+  */
+  normal_distribution<double> dist_x(x, std[0]);
+  normal_distribution<double> dist_y(y, std[1]);
+  normal_distribution<double> dist_theta(theta, std[2]);
 
+  for(int i=0; i<num_particles; ++i){
+    Particle p;
+    p.id=i;
+    //sample from the normal distributions using gen
+    p.x=dist_x(gen);
+    p.y=dist_y(gen);
+    p.theta=dist_theta(gen);
+    p.weight=1.0;
+
+    particles.push_back(p);
+  }
+  is_initialized=true;
 }
 
 void ParticleFilter::prediction(double delta_t, double std_pos[], 
